bb72416ffa492c4cf9085c80064a757acd66f7d9
Douglas Hewitt <madebydouglas@gmail.com>
2020-01-03 13:20:39 -0800

    added appkit feather framework

---
+++ feather/featherAppKit/featherAppKit.h
@@ -0,0 +1,19 @@
+//
+//  featherAppKit.h
+//  featherAppKit
+//
+//  Created by Douglas Hewitt on 1/3/20.
+//  Copyright © 2020 Douglas Hewitt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for featherAppKit.
+FOUNDATION_EXPORT double featherAppKitVersionNumber;
+
+//! Project version string for featherAppKit.
+FOUNDATION_EXPORT const unsigned char featherAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <featherAppKit/PublicHeader.h>
+
+
