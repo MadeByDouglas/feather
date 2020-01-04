b7f89b08169172c9de18fd8454bc68aa9557b612
Douglas Hewitt <madebydouglas@gmail.com>
2020-01-04 11:24:56 -0800

    added tests for ios library

---
+++ feather/FeatherTests/FeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  FeatherTests.swift
+//  FeatherTests
+//
+//  Created by Douglas Hewitt on 1/4/20.
+//  Copyright © 2020 Douglas Hewitt. All rights reserved.
+//
+
+import XCTest
+
+class FeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
